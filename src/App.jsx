be8b8e8a5615ff8b5142dcbd7b18db7ec1a4848c
Pticaed <alexchmilsoftware@gmail.com>
2025-07-08 17:46:12 +0300

    v1 mainNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // исправлено: 'react-router-dom', а не 'react-router'
 import './App.css'
+import { MainNavBar } from './components/mainNavBar/mainNavBar'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainNavBar />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/mainNavBar/mainNavBar.jsx
@@ -0,0 +1,13 @@
+import "./mainNavBar.css"
+
+export function MainNavBar()
+{
+    return (
+        <header>
+            <p id="headerPromote">ТОТАЛЬНИЙ РОЗПРОДАЖ ДО -50%</p>
+            <nav id="mainNav">
+                
+            </nav>
+        </header>
+    )
+}
