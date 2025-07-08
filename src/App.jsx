b6b2fa9e3d2dd8c881f703e6b128ccfd14660c4a
Vova <lololoskafry@gmail.com>
2025-07-08 19:08:05 +0300

    added green-btn, started leftbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { LeftbarMainPage } from './components/left-bar-mainpage/leftbar'
 
 function App() {
   return (
     <>
-      
+      <LeftbarMainPage></LeftbarMainPage>
     </>
   )
 }
