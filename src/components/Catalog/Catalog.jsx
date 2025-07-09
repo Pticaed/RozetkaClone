616f4b0d746a6b8f5ef5c1959e6e2d43747526de
Pticaed <alexchmilsoftware@gmail.com>
2025-07-09 19:38:37 +0300

    without DefaultBlock css

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react"
+import "./Catalog.css"
+
+export function Catalog()
+{
+    return (
+        <div id="divCatalog">
+            <div id="leftDiv" className="default-block">
+                f
+            </div>
+
+            <div id="rightDiv" className="default-block">
+               f 
+            </div>
+        </div>
+    )
+}
