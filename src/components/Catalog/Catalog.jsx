c5a12af0c3f8a0b46a5877b758e307e0e0285ce5
Pticaed <alexchmilsoftware@gmail.com>
2025-07-09 23:56:00 +0300

    MainNavBar with catalog v1

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,17 +1,39 @@
 // import { useState } from "react"
 import "./Catalog.css"
+import { Modal } from "react-bootstrap"
 
-export function Catalog()
+function PartSectionCatalog(props)
 {
     return (
-        <div id="divCatalog">
-            <div id="leftDiv" className="default-block">
-                f
-            </div>
-
-            <div id="rightDiv" className="default-block">
-               f 
-            </div>
+        <div class="partSectionCatalog">
+            <p id="partSectionCatalogTitle"></p>
+            {props.listSubsection.map(el => {
+                <Link to="">{el}</Link>
+            })}
         </div>
+    )
+}
+
+export function Catalog(props)
+{
+    if(!props.dialog)
+    {
+        return null
+    }
+    return (
+            <div id="backdropCatalog" onClick={() => props.setDialog(false)}>
+                <Modal show={props.dialog} onHide={() => props.setDialog(false)} backdrop={false}>
+                    <Modal.Body>
+                        <div id="divCatalog" className="d-flex">
+                            <div id="leftDiv" className="default-block">
+                                f
+                            </div>
+                            <div id="rightDiv" className="default-block">
+                                f 
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+            </div>
     )
 }
