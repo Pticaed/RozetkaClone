459d596cae248d2969b726a6abcfce2ed4aa5a89
Vova <lololoskafry@gmail.com>
2025-07-13 23:10:29 +0300

    added household page, fixed drop menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom' // исправлено: 'react-router-dom', а не 'react-router'
 import './App.css'
 import { MainNavBarCatalogCombo } from './components/Combo/MainNavBarCatalogCombo'
-import { HouseholdApps } from './components/HouseholdAppliances/Household'
+
 
 
 
 function App() {
   return (
-    <BrowserRouter>
+     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HouseholdApps />} />
+        <Route path="/" element={<MainNavBarCatalogCombo />} />
       </Routes>
     </BrowserRouter>
   )
