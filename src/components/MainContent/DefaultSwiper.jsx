e25ce617d22448ea876284d3f16fa54c4bd4a8dc
dgalytskyi <galitskiy.dima8@gmail.com>
2025-07-13 17:59:18 +0200

    added slides and slidesPerView

--- src/components/MainContent/DefaultSwiper.jsx
+++ src/components/MainContent/DefaultSwiper.jsx
@@ -7,12 +7,12 @@ import "swiper/scss/autoplay";
 import "swiper/scss/free-mode";
 import "./DefaultSwiper.scss"
 
-export function DefaultSwiper() {
+export function DefaultSwiper({slidesPerView, slides}) {
   return (
     <Swiper className="default-swiper"
             modules={[Autoplay, FreeMode]}
             freeMode={true}
-            spaceBetween={5}
+            spaceBetween={10}
             autoplay={{
               delay: 5000,
               disableOnInteraction: true
@@ -40,49 +40,46 @@ export function DefaultSwiper() {
               }
             }}
             loop={true}
-            slidesPerView={5}>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/appleIphone2.png"
-          title="Мобільний телефон Apple iPhone 13 Pro Max 256 GB Sierra Blue"
-          previousPrice="47 499" price="46 499" priceStyle="discount"/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/ps5.png"
-          title="Ігрова приставка PS5 PlayStation 5 Digital Edition"
-          price="17 899"/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/philipsCoffeeMachine.png"
-          title="Кавомашина PHILIPS Series 2200 EP2235/40"
-          price="17 499" previousPrice="17 999" priceStyle="discount"/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/ps5.png"
-          title="Ігрова приставка PS5 PlayStation 5 Digital Edition"
-          price="17 899" priceStyle="discount"/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/ps5.png"
-          title="Ігрова приставка PS5 PlayStation 5 Digital Edition"
-          price="17 899" priceStyle="discount"/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <ProductCard
-          type="normal-adaptive-product-card default-width-product-card"
-          img="/resourses/maincontent/ps5.png"
-          title="Ігрова приставка PS5 PlayStation 5 Digital Edition"
-          price="17 899" priceStyle="discount"/>
-      </SwiperSlide>
+            slidesPerView={slidesPerView}>
+      {slides.map((slide, index) => {
+        return (<SwiperSlide>
+                  <ProductCard
+                    type={slide.type}
+                    img={slide.img}
+                    title={slide.title}
+                    previousPrice={slide.previousPrice}
+                    price={slide.price}
+                    priceStyle={slide.priceStyle}/>
+                </SwiperSlide>)
+      })}
+      {/*<SwiperSlide>*/}
+      {/*  <ProductCard*/}
+      {/*    type="normal-adaptive-product-card default-width-product-card"*/}
+      {/*    img="/resourses/maincontent/philipsCoffeeMachine.png"*/}
+      {/*    title="Кавомашина PHILIPS Series 2200 EP2235/40"*/}
+      {/*    price="17 499" previousPrice="17 999" priceStyle="discount"/>*/}
+      {/*</SwiperSlide>*/}
+      {/*<SwiperSlide>*/}
+      {/*  <ProductCard*/}
+      {/*    type="normal-adaptive-product-card default-width-product-card"*/}
+      {/*    img="/resourses/maincontent/ps5.png"*/}
+      {/*    title="Ігрова приставка PS5 PlayStation 5 Digital Edition"*/}
+      {/*    price="17 899" priceStyle="discount"/>*/}
+      {/*</SwiperSlide>*/}
+      {/*<SwiperSlide>*/}
+      {/*  <ProductCard*/}
+      {/*    type="normal-adaptive-product-card default-width-product-card"*/}
+      {/*    img="/resourses/maincontent/ps5.png"*/}
+      {/*    title="Ігрова приставка PS5 PlayStation 5 Digital Edition"*/}
+      {/*    price="17 899" priceStyle="discount"/>*/}
+      {/*</SwiperSlide>*/}
+      {/*<SwiperSlide>*/}
+      {/*  <ProductCard*/}
+      {/*    type="normal-adaptive-product-card default-width-product-card"*/}
+      {/*    img="/resourses/maincontent/ps5.png"*/}
+      {/*    title="Ігрова приставка PS5 PlayStation 5 Digital Edition"*/}
+      {/*    price="17 899" priceStyle="discount"/>*/}
+      {/*</SwiperSlide>*/}
     </Swiper>
   );
 }
